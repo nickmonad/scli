069ab910247520489e00b66475bd5634f2ed5a4a
ngmiller <nick@ngmiller.io>
2019-12-26 17:49:25 -0600

    client: fix stream url resolution

--- src/soundcloud.rs
+++ src/soundcloud.rs
@@ -56,28 +56,35 @@ impl Client {
     }
 
     pub fn stream(&self, stream_url: String) -> Result<File, reqwest::Error> {
-        self.resolve(stream_url).and_then(|location: String| {
-            // get raw audio from resolved resource
-            let mut resp = self.client.get(&location).send().unwrap();
+        // resolve stream url
+        let mut resolve_resp = self
+            .client
+            .get(&stream_url)
+            .header(header::USER_AGENT, "scli")
+            .query(&[("oauth_token", &self.oauth)])
+            .send()?;
 
-            // create a temporary file on disk and spawn a thread to write to it
-            let mut writer = File::create("stream.mp3").unwrap();
-            let reader = File::open("stream.mp3").unwrap();
+        // get raw audio from resolved resource
+        let resource: Resource = resolve_resp.json()?;
+        let mut resp = self.client.get(&resource.location).send().unwrap();
 
-            // TODO(ngmiller)
-            // This is terribly hacky. It seems the returned reader file handle
-            // doesn't handle the writing very well when stream.mp3 doesn't exist
-            // and causes the player thread to error out with an unrecognized format,
-            // so we need to sleep a bit after starting the writer thread.
-            // Ideally, this is all buffered in memory and we don't have to use a file
-            // to coordinate.
-            thread::spawn(move || {
-                resp.copy_to(&mut writer).unwrap();
-            });
+        // create a temporary file on disk and spawn a thread to write to it
+        let mut writer = File::create("stream.mp3").unwrap();
+        let reader = File::open("stream.mp3").unwrap();
 
-            thread::sleep(time::Duration::from_millis(100));
-            Ok(reader)
-        })
+        // TODO(ngmiller)
+        // This is terribly hacky. It seems the returned reader file handle
+        // doesn't handle the writing very well when stream.mp3 doesn't exist
+        // and causes the player thread to error out with an unrecognized format,
+        // so we need to sleep a bit after starting the writer thread.
+        // Ideally, this is all buffered in memory and we don't have to use a file
+        // to coordinate.
+        thread::spawn(move || {
+            resp.copy_to(&mut writer).unwrap();
+        });
+
+        thread::sleep(time::Duration::from_millis(100));
+        Ok(reader)
     }
 
     fn resolve(&self, url: String) -> Result<String, reqwest::Error> {
@@ -90,7 +97,7 @@ impl Client {
             .query(&[("url", url)])
             .send()?;
 
-        let resolved: Resource = resp.json()?;
-        Ok(resolved.location)
+        let resource: Resource = resp.json()?;
+        Ok(resource.location)
     }
 }
